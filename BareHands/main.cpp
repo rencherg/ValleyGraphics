0f8cc825d6fa94b5f533c12d931ea5a07ba1b390
rencherg <rencher.grant@gmail.com>
2020-11-15 16:11:17 -0700

    Initial Commit

---
+++ BareHands/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BareHands
+//
+//  Created by Grant Alan Rencher on 11/15/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
